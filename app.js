2cf242cc85b9419a054b61436d71e049c54c91a7
Anurag-NURA <anuragrawatz3k@gmail.com>
2022-12-12 19:41:20 +0530

    website done

--- app.js
+++ app.js
@@ -1,46 +1,173 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const e = require("express");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const aboutContent = "This page is all about the technologies used in creating the website. Needless to say but this is a team project created for only education purposes."
 const app = express();
-
-let posts = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+app.use(session({
+  secret: "Something secret is here.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+mongoose.connect("mongodb://localhost:27017/blogDB")
+  .then(() => {
+    console.log("Connection established!!!");
+  })
+  .catch((err) => { console.log(err); });
+
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+const blogSchema = new mongoose.Schema({
+  author: String,
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+});
+const Post = mongoose.model('Post', blogSchema);
+
+
 //for home page
 app.get("/", (req, res) => {
-  //renders home.ejs file on "/" page
-  res.render("home", {
-    startingContent: homeStartingContent,
-    homePosts: posts
-  });
+  if (req.isAuthenticated()) {
+    //renders home.ejs file on "/" page
+    Post.find({}, (err, posts) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("home", {
+          startingContent: homeStartingContent,
+          homePosts: posts
+        });
+      }
+    })
+  } else {
+    res.redirect("/login");
+  }
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.get("/posts/:topic", (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.topic);
-  posts.forEach(element => {
-    const storedTitle = _.lowerCase(element.title);
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        postTitle: element.title,
-        postContent: element.content
-      });
+app.post("/register", (req, res) => {
+  User.register({
+    username: req.body.username,
+  }, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/");
+      })
     }
   });
 });
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect('/');
+      });
+    }
+  })
+});
+
+
+
+app.get("/posts/:postId", (req, res) => {
+  const requestedId = req.params.postId;
+
+  if (req.isAuthenticated()) {
+    Post.findOne({ _id: requestedId }, (err, post) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("post", {
+          postTitle: post.title,
+          postContent: post.content
+        })
+      };
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/posts/:postId", (req, res) => {
+  Post.findOne({ _id: req.params.postId }, (err, post) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (post.author === req.session.passport.user) {
+        Post.deleteOne({ _id: req.params.postId }, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("message", {
+              messageTitle: "Post deleted",
+              messageContent: "The post has been deleted successfully"
+            });
+          }
+        })
+      } else {
+        res.render("message", {
+          messageTitle: "Unauthorized Action",
+          messageContent: "You are not the creator of this post"
+        });
+      }
+    }
+  });
+});
+
 
 
 //for about page
@@ -52,35 +179,48 @@ app.get("/about", (req, res) => {
 });
 
 
-//for contact page
-app.get("/contact", (req, res) => {
-  //renders contact.ejs file on "/contact" page
-  res.render("contact", {
-    CContent: contactContent
+
+//for compose page
+app.get("/compose", (req, res) => {
+  if (req.isAuthenticated()) {
+    //renders compose.ejs file on "/compose" page
+    res.render("compose");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/compose", async (req, res) => {
+
+  const composePost = new Post({
+    author: req.session.passport.user,
+    title: req.body.composeText,
+    content: req.body.composePost
+  });
+
+  await composePost.save((err) => {
+    if (err) {
+      //if saving fails
+      console.log(err);
+    } else {
+      //than redirected to the home page to display the composed post
+      res.redirect("/");
+    }
   });
 });
 
 
-//for compose page
-app.get("/compose", (req, res) => {
-  //renders compose.ejs file on "/compose" page
-  res.render("compose");
-});
 
-app.post("/compose", (req, res) => {
-  //created an object for storing the title and text of the compose page 
-  const composePost = {
-    title: req.body.composeText,
-    content: req.body.composePost
-  };
-
-  //pushed that object into an array having name 'posts'
-  posts.push(composePost);
-  //than redirected to the home page to display the composed post
+app.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+  });
   res.redirect("/");
 });
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(8000, function () {
+  console.log("Server started on port 8000");
 });
